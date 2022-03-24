04b5732e765753f141482668a484df50ebf14b49
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:27:18 +0530

    Add project files.

---
+++ TSystem_Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER THE NO YOU WANT FACTOTIAL OF");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int fact = 1;
+            for(i=1;i<=num;i++)
+            {
+                fact = fact * i;
+                Console.WriteLine("FACTORIAL OF"+num+ "="+fact);
+            }
+            Console.ReadLine();
+        }
+    }
+}
